3ea4e5f50ae66243052259c70b934aa90ca02c0d
Imsleepy <161543242+Imsleepybob@users.noreply.github.com>
2024-12-02 22:53:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,9 +79,8 @@ def get_month_dates():
 
 def get_week_dates():
     today = datetime.now().date()
-    start_of_week = today - timedelta(days=today.weekday()) + timedelta(days=6)
+    start_of_week = today - timedelta(days=today.weekday())
     dates = [(start_of_week + timedelta(days=i)).strftime('%Y%m%d') for i in range(7)]
-    print(f"Today: {today}, Weekday: {today.weekday()}, Start of week: {start_of_week}, Dates: {dates}")
     return dates
 
 def get_month_meals(school_code, region_code):
