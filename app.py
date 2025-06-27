bce043ec94b923a909776d73cada9878c9f67407
Imsleepy <161543242+Imsleepybob@users.noreply.github.com>
2025-06-28 01:52:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -705,7 +705,7 @@ def namufile1():
 
 @app.route("/sitemap.xml")
 def sitemap():
-    return send_from_directory(directory=os.getcwd(), path="sitemap.xml", mimetype="application/xml")
+    return send_file("sitemap.xml", mimetype="application/xml")
 
 # --- 탬퍼몽키 스크립트 ---
 STATIC_DIR = app.root_path
